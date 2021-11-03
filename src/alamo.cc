e89582a15796475261bd5980cb2f4dff6193603d
Brandon Runnels <brunnels@uccs.edu>
2021-11-03 13:18:12 -0600

    Fixed whitespace issues

--- src/alamo.cc
+++ src/alamo.cc
@@ -22,97 +22,97 @@
 
 int main (int argc, char* argv[])
 {
-	Util::Initialize(argc,argv);
+    Util::Initialize(argc,argv);
 
-	std::string program = "microstructure";
-	amrex::ParmParse pp("alamo");
-	pp.query("program",program);
+    std::string program = "microstructure";
+    amrex::ParmParse pp("alamo");
+    pp.query("program",program);
 
-	if (program == "microstructure")
-	{
-		srand(2);
-		Integrator::Integrator *pfm = new Integrator::PhaseFieldMicrostructure();
-		//Integrator::PhaseFieldMicrostructure pfm;
-		pfm->InitData();
-		pfm->Evolve();
-		delete pfm;
-	}
-	else if (program == "eshelby")
-	{
-		Integrator::Integrator *eshelby = new Integrator::TensionTest<Model::Solid::Affine::Isotropic>();
-		eshelby->InitData();
-		eshelby->Evolve();		
-		delete eshelby;
-	}
-	else if (program == "crystalplastictensiontest")
-	{
-		Integrator::Integrator *cp_tt = new Integrator::TensionTest<Model::Solid::Affine::CrystalPlastic>();
-		cp_tt->InitData();
-		cp_tt->Evolve();		
-		delete cp_tt;
-	}
-	else if (program == "sgcptensiontest")
-	{
-		Integrator::Integrator *sgcp_tt = new Integrator::TensionTestSGCP<Model::Solid::Affine::StrainGradientCrystalPlastic>();
-		sgcp_tt->InitData();
-		sgcp_tt->Evolve();		
-		delete sgcp_tt;
-	}
-	else if (program == "j2plastictensiontest")
-	{
-		Integrator::Integrator *j2_tt = new Integrator::TensionTest<Model::Solid::Affine::J2PlasticDegradable>();
-		j2_tt->InitData();
-		j2_tt->Evolve();		
-		delete j2_tt;
-	}
-	else if (program == "finitekinematics")
-	{
-		//Integrator::Integrator *fk = new Integrator::FiniteKinematics();
-		Integrator::Integrator *fk = new Integrator::TensionTest<Model::Solid::Elastic::NeoHookean>();
-		fk->InitData();
-		fk->Evolve();		
-		delete fk;
-	}
-	else if (program == "flame")
-	{
-		Integrator::Integrator *flame = new Integrator::Flame();
-		flame->InitData();
-		flame->Evolve();
-		delete flame;
-	}
-	else if (program == "heat")
-	{
-		Integrator::Integrator *heatconduction = new Integrator::HeatConduction();
-		heatconduction->InitData();
-		heatconduction->Evolve();
-		delete heatconduction;
-	}
-	else if (program == "degradation")
-	{
-		srand(1.0*amrex::ParallelDescriptor::MyProc());
-		Integrator::PolymerDegradation model;
-		model.InitData();
-		model.Evolve();
-		//delete model;
-	}
-	else if (program == "fracture")
-	{
-		srand(1.0*amrex::ParallelDescriptor::MyProc());
-		Integrator::Fracture model;
-		model.InitData();
-		model.Evolve();
-		//delete model;
-	}
-	else if (program == "trigtest")
-	{
-		Test::Operator::Elastic test;
-		test.Define(32,1);
-		test.TrigTest(0,0,1,Util::GetFileName());
-	}
-	else
-	{
-		Util::Abort(INFO,"Error: \"",program,"\" is not a valid program.");
-	}
+    if (program == "microstructure")
+    {
+        srand(2);
+        Integrator::Integrator *pfm = new Integrator::PhaseFieldMicrostructure();
+        //Integrator::PhaseFieldMicrostructure pfm;
+        pfm->InitData();
+        pfm->Evolve();
+        delete pfm;
+    }
+    else if (program == "eshelby")
+    {
+        Integrator::Integrator *eshelby = new Integrator::TensionTest<Model::Solid::Affine::Isotropic>();
+        eshelby->InitData();
+        eshelby->Evolve();        
+        delete eshelby;
+    }
+    else if (program == "crystalplastictensiontest")
+    {
+        Integrator::Integrator *cp_tt = new Integrator::TensionTest<Model::Solid::Affine::CrystalPlastic>();
+        cp_tt->InitData();
+        cp_tt->Evolve();        
+        delete cp_tt;
+    }
+    else if (program == "sgcptensiontest")
+    {
+        Integrator::Integrator *sgcp_tt = new Integrator::TensionTestSGCP<Model::Solid::Affine::StrainGradientCrystalPlastic>();
+        sgcp_tt->InitData();
+        sgcp_tt->Evolve();        
+        delete sgcp_tt;
+    }
+    else if (program == "j2plastictensiontest")
+    {
+        Integrator::Integrator *j2_tt = new Integrator::TensionTest<Model::Solid::Affine::J2PlasticDegradable>();
+        j2_tt->InitData();
+        j2_tt->Evolve();        
+        delete j2_tt;
+    }
+    else if (program == "finitekinematics")
+    {
+        //Integrator::Integrator *fk = new Integrator::FiniteKinematics();
+        Integrator::Integrator *fk = new Integrator::TensionTest<Model::Solid::Elastic::NeoHookean>();
+        fk->InitData();
+        fk->Evolve();        
+        delete fk;
+    }
+    else if (program == "flame")
+    {
+        Integrator::Integrator *flame = new Integrator::Flame();
+        flame->InitData();
+        flame->Evolve();
+        delete flame;
+    }
+    else if (program == "heat")
+    {
+        Integrator::Integrator *heatconduction = new Integrator::HeatConduction();
+        heatconduction->InitData();
+        heatconduction->Evolve();
+        delete heatconduction;
+    }
+    else if (program == "degradation")
+    {
+        srand(1.0*amrex::ParallelDescriptor::MyProc());
+        Integrator::PolymerDegradation model;
+        model.InitData();
+        model.Evolve();
+        //delete model;
+    }
+    else if (program == "fracture")
+    {
+        srand(1.0*amrex::ParallelDescriptor::MyProc());
+        Integrator::Fracture model;
+        model.InitData();
+        model.Evolve();
+        //delete model;
+    }
+    else if (program == "trigtest")
+    {
+        Test::Operator::Elastic test;
+        test.Define(32,1);
+        test.TrigTest(0,0,1,Util::GetFileName());
+    }
+    else
+    {
+        Util::Abort(INFO,"Error: \"",program,"\" is not a valid program.");
+    }
 
-	Util::Finalize();
+    Util::Finalize();
 } 
